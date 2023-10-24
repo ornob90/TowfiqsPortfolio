b4f9cf3b61c93000a57e9f66fa566794cdbc29c7
ornob90 <oornob49@gmail.com>
2023-10-24 08:34:56 +0600

    about cell complete of grid

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import Container from "../../components/shared/Container";
 import SectionHeader from "../../components/shared/SectionHeader";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import SpanBold from "../../components/shared/SpanBold";
 
 const About = () => {
   return (
     <Container>
       <SectionHeader name="ABOUT" />
-      <div className="min-h-[400px] grid grid-cols-8 grid-rows-10 grid-flow-row-dense mb-12">
+      <div className="min-h-[400px] grid grid-cols-8 grid-rows-10 grid-flow-row-dense mb-12 gap-1">
         {/* image cell */}
         <div className="border border-black col-span-2 row-span-8 image">
           Image
         </div>
 
         {/* about cell */}
-        <div className="border border-black col-span-4 row-span-4 about">
-          About
+        <div className="border border-black col-span-4 row-span-4 about bg-black-dark text-white px-4 pt-4 rounded-lg">
+          <div className="flex justify-between mb-6">
+            <h1 className="text-3xl font-generalBold">I’m Towfiq</h1>
+            <FaArrowRightFromBracket />
+          </div>
+          <p className="font-generalLight">
+            “Emerging <SpanBold> web developer</SpanBold> with{" "}
+            <SpanBold> good fundamentals</SpanBold>, turning design into
+            <SpanBold> user-friendly websites</SpanBold>. Committed to
+            delivering
+            <SpanBold> visually appealing</SpanBold> solutions. Eager to apply
+            skills, <SpanBold> collaborate</SpanBold>, and create exceptional
+            online experiences.”
+          </p>
         </div>
 
         {/* project cell */}
---
+++ src/components/shared/SpanBold.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpanBold = ({ children }) => {
+  return <span className="font-generalBold">{children}</span>;
+};
+
+export default SpanBold;

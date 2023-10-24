3b4cfc9056548a73b6c572eabd0b8771b003a06f
ornob90 <oornob49@gmail.com>
2023-10-24 15:11:34 +0600

    about grid responsive for mobile device

--- src/Sections/About/AboutMD.jsx
+++ src/Sections/About/AboutMD.jsx
@@ -17,18 +17,43 @@ const AboutMD = () => {
   return (
     <Container className="lg:hidden">
       <SectionHeader name="ABOUT" />
-      <div className="min-h-[400px] grid grid-cols-8 grid-rows-10 grid-flow-row-dense mb-12 gap-2">
+      <div className="min-h-[400px] grid grid-cols-1 md:grid-cols-8 md:grid-rows-10 mb-12 gap-2">
         {/* image cell */}
-        <div className="order-0 row-span-8 image-md">
+        <div className="h-[300px]  md:h-auto -order-1 md:order-0 image-md hidden md:block">
           <img
             src="./assets/myPic.png"
             alt=""
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+
+        <div className="h-[300px]  md:h-auto -order-1 md:order-0  md:hidden">
+          <img
+            src="./assets/myPic.png"
+            alt=""
+            className="w-full h-full object-cover rounded-lg"
           />
         </div>
 
         {/* about cell */}
-        <div className="order-1 border border-black  about-md bg-black-dark text-white px-3 pt-3 rounded-lg">
+        <div className="hidden md:block -order-2 md:order-1 border border-black  about-md bg-black-dark text-white px-3 pt-3 rounded-lg">
+          <div className="flex justify-between mb-6">
+            <h2 className="text-2xl font-generalBold">I'M TOWFIQ</h2>
+            <FaArrowRightFromBracket className="text-2xl " />
+          </div>
+          <p className="font-generalLight text-sm lg:text-base p-1">
+            “Emerging <SpanBold> web developer</SpanBold> with{" "}
+            <SpanBold> good fundamentals</SpanBold>, turning design into
+            <SpanBold> user-friendly websites</SpanBold>. Committed to
+            delivering
+            <SpanBold> visually appealing</SpanBold> solutions. Eager to apply
+            skills, <SpanBold> collaborate</SpanBold>, and create exceptional
+            online experiences.”
+          </p>
+        </div>
+
+        {/* about cell */}
+        <div className="md:hidden -order-2 md:order-1 border border-black  bg-black-dark text-white px-3 pt-3 rounded-lg">
           <div className="flex justify-between mb-6">
             <h2 className="text-2xl font-generalBold">I'M TOWFIQ</h2>
             <FaArrowRightFromBracket className="text-2xl " />
@@ -45,7 +70,7 @@ const AboutMD = () => {
         </div>
 
         {/* project cell */}
-        <div className="order-6 border border-black  project-md bg-black-gray text-white rounded-lg p-3 ">
+        <div className="order-6 border border-black  project-md bg-black-gray text-white rounded-lg p-3 hidden md:block">
           <div className="h-max flex flex-col gap-2">
             <h2 className="text-2xl font-generalBold">PROJECTS</h2>
             <p className="text-light-gray text-[12px] font-generalBold">
@@ -77,7 +102,7 @@ const AboutMD = () => {
         </div>
 
         {/* skill cell */}
-        <div className="order-3 border border-black  skills-md p-3 bg-black-gray text-white rounded-lg flex flex-col justify-between ">
+        <div className=" -order-1 md:order-3 border border-black  skills-md p-3 bg-black-gray text-white rounded-lg  flex-col justify-between hidden md:flex">
           <div className="flex justify-between mb-6">
             <h2 className="text-2xl font-generalBold">SKILLS</h2>
             <FaArrowRightFromBracket className="text-2xl " />
@@ -98,7 +123,27 @@ const AboutMD = () => {
         </div>
 
         {/* social cell */}
-        <div className="order-4 border border-black  social-md bg-black-dark text-white p-3  flex justify-between flex-col rounded-lg">
+        <div className=" -order-1 md:order-4 border border-black  social-md bg-black-dark text-white p-3  justify-between flex-col rounded-lg hidden md:flex ">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-generalBold">SOCIAL</h2>
+            <p className="text-light-gray text-[12px] font-generalBold">
+              Explore My Online Presence and Connect with Me on Social Platforms
+            </p>
+          </div>
+          <div className="flex gap-2 items-center pt-2 lg:pt-0">
+            <div className=" bg-white text-3xl lg:text-5xl text-black rounded-md p-1">
+              <FaGithub />
+            </div>
+            <div className=" text-lg lg:text-4xl rounded-md p-3 bg-black text-white">
+              <SlSocialLinkedin />
+            </div>
+            <div className=" bg-white text-3xl lg:text-5xl text-black rounded-md p-1">
+              <BiLogoFacebookCircle />
+            </div>
+          </div>
+        </div>
+
+        <div className=" -order-1 md:order-4 border border-black  bg-black-dark text-white p-3  justify-between flex-col rounded-lg flex md:hidden ">
           <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-generalBold">SOCIAL</h2>
             <p className="text-light-gray text-[12px] font-generalBold">
@@ -119,7 +164,7 @@ const AboutMD = () => {
         </div>
 
         {/* resume cell */}
-        <div className="order-5 border border-black  resume-md font-generalBold flex justify-between flex-col p-2 bg-black-dark rounded-lg text-white">
+        <div className="order-5 border border-black  resume-md font-generalBold  justify-between flex-col p-2 bg-black-dark rounded-lg text-white hidden md:flex">
           <BsFillPersonLinesFill className="text-2xl" />
           <p className="text-light-gray text-[12px]"> LEARN MORE ABOUT ME</p>
           <div className="flex justify-between items-center">
@@ -132,7 +177,7 @@ const AboutMD = () => {
         </div>
 
         {/* contact cell */}
-        <div className="order-6 border border-black  contact-md bg-black-gray text-white p-3 rounded-lg flex flex-col justify-between lg:block">
+        <div className="order-6 border border-black  contact-md bg-black-gray text-white p-3 rounded-lg  flex-col justify-between lg:block hidden md:flex">
           <h2 className="text-2xl font-generalBold pb-4">GET IN TOUCH</h2>
           <div className="flex justify-between items-center text-xl">
             <div className="flex gap-2 items-center">

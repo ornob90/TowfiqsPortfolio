82674941d1070f229c99596db25eec950b78b8e3
ornob90 <oornob49@gmail.com>
2023-10-24 07:39:55 +0600

    about section grid layout design complete

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -6,7 +6,42 @@ const About = () => {
   return (
     <Container>
       <SectionHeader name="ABOUT" />
-      <div className="bg-yellow-500 h-screen"></div>
+      <div className="min-h-[400px] grid grid-cols-8 grid-rows-10 grid-flow-row-dense mb-12">
+        {/* image cell */}
+        <div className="border border-black col-span-2 row-span-8 image">
+          Image
+        </div>
+
+        {/* about cell */}
+        <div className="border border-black col-span-4 row-span-4 about">
+          About
+        </div>
+
+        {/* project cell */}
+        <div className="border border-black col-span-2 row-span-6 project">
+          Project
+        </div>
+
+        {/* skill cell */}
+        <div className="border border-black col-span-4 row-span-4 skills">
+          Skill
+        </div>
+
+        {/* social cell */}
+        <div className="border border-black col-span-2 row-span-4 social">
+          Social
+        </div>
+
+        {/* resume cell */}
+        <div className="border border-black col-span-2 row-span-2 resume">
+          Resume
+        </div>
+
+        {/* contact cell */}
+        <div className="border border-black col-span-4 row-span-2 contact">
+          Contact
+        </div>
+      </div>
     </Container>
   );
 };
